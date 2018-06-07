775f8f1cc3b47a6ae588718817b4992c42879f26
madhurima-beryl <madhurima.chhabra@berylsystems.com>
2018-06-07 15:15:32 +0530

    working blog model

--- lets_blog/app/controllers/blogs_controller.rb
+++ lets_blog/app/controllers/blogs_controller.rb
@@ -19,7 +19,8 @@ class BlogsController < ApplicationController
     @blog = Blog.new
     if @blog.save(blog_params)
       flash[:notice] = "Successfully created blog!"
-      redirect_to blog_path(@blog)
+      redirect_to blog_path(@blog)    #show action 
+      #redirect_to @blog              #show action
     else
       flash[:alert] = "Error creating new blog!"
       render :new
@@ -39,7 +40,7 @@ class BlogsController < ApplicationController
   def destroy
     if @blog.destroy
       flash[:notice] = "Successfully deleted blog!"
-      redirect_to blogs_path
+      redirect_to blogs_path    #index action
     else
       flash[:alert] = "Error updating blog!"
     end
@@ -51,7 +52,7 @@ class BlogsController < ApplicationController
     params.require(:blog).permit(:title, :description)
   end
 
-  def blog_post
+  def find_blog
     @blog = Blog.find(params[:id])
   end
 
